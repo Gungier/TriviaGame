aeceacca55956f367736a6b6a5081a0b26334a5e
Gabriel Ungier <gabe@Gabriels-MacBook-Pro.local>
2019-02-28 18:35:33 -0500

    mostly working, need fix pics and some buttons

--- assets/javascript/app2.0.js
+++ assets/javascript/app2.0.js
@@ -30,27 +30,22 @@ $(window).on('resize', checkForResize);
 
 checkForResize();
 
-var gameArea = $('#gameSpace');
-var secLeft = 30;
-
+var gameArea = $('#quizSpace');
+var startCounter = 30;
 
 //Click Events
 
-$(document).on('click' + 'touch', '.goAgain', function (e) {
-    e.preventDefault();
+$(document).on('click', '.goAgain', function (e) {
     quiz.reset();
 });
 
-$(document).on('click touch', '.answerBtn', function (e) {
-    e.preventDefault();
+$(document).on('click', '.answerBtn', function (e) {
     quiz.clicked(e);
 });
 
-$(document).on('click touch', '#start', function (e) {
-    $('#timer').text('Time Remaining: ' +  quiz.counter);
+$(document).on('click', '#begin', function (e) {
+    $('.timer').append("<h2>Time Remaining: <span id='numeric-counter'>30</span> Seconds </h2>");
     quiz.startTrivia();
-    quiz.countdown();
-    console.log("timer: " + quiz.startTrivia);
 });
 
 //Trivia Questions
@@ -74,48 +69,50 @@ var questions = [{
     question: "Who was the the first alterego of the Flash?",
     answers: ["Alan Scott", "Jay Garrick", "Clark Kent", "Barry Allen"],
     rightAnswer: "Jay Garrick",
-    image: "assets/images/theFlash.jpeg",
+    image: "assets/images/theFlash.jpg",
 }];
 
 var quiz = {
     questions: questions,
     currentQuestion: 0,
-    counter: secLeft,
-    correct: 0,
+    counter: startCounter,
+    corret: 0,
     incorrect: 0,
     countdown: function () {
-        quiz.counter --;
-        $("#game-timer").text("Time Remaining: " + quiz.counter);
-
-        console.log("Timer:" + quiz.counter)
+        quiz.counter--;
+        $('numeric-counter').html(quiz.counter);
 
         if (quiz.counter === 0) {
             console.log("TIME'S UP");
             quiz.timesUp();
-            console.log(countdown)
         }
     },
     startTrivia: function () {
         timer = setInterval(quiz.countdown, 1000);
         gameArea.html('<h2>' + questions[this.currentQuestion].question + '</h2>');
-        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-            gameArea.append("<button class='answerBtn' id='button'"+ 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i] + '</button>');
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) 
+        
+        {
+            var button = $("<button>")
+            button.attr({
+                "class": 'answerBtn',
+                "data-name": questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i] 
+            });
         }
 
     },
     nextQuestion: function () {
-        quiz.counter = secLeft;
-        $('#game-timer').html("Time Remaining: " + quiz.counter);
+        quiz.counter = startCounter;
+        $('#numeric-counter').html(quiz.counter);
         quiz.currentQuestion++;
         quiz.startTrivia();
     },
     timesUp: function () {
         clearInterval(timer);
-        $('#game-timer').html("Time Remaining: " + quiz.counter);
+        $('#numeric-counter').html(quiz.counter);
 
         gameArea.html('<h2>Times Up</h2>');
-        gameArea.append('<h3>The Right Choice was: ' + questions[this.currentQuestion].rightAnswer); 
-        gameArea.append('img src="' + questions[this.currentQuestion].image + '"/>');
+        gameArea.append('<h3>The Right Choice was: ' + questions[this.currentQuestion].rightAnswer); gameArea.append('img src="' + questions[this.currentQuestion].image + '"/>');
 
         if (quiz.currentQuestion === questions.length - 1) {
             setTimeout(quiz.results, 3000);
@@ -127,19 +124,19 @@ var quiz = {
     results: function () {
         clearInterval(timer);
 
-        gameArea.html('<h2>Well done, here is your final score!</h2>');
-        $('#game-timer').html(quiz.counter);
-        gameArea.append('<h3>Correct Answers: ' + quiz.correct + '</h3>');
-        gameArea.append('<h3>Incorrect Answers: ' + quiz.incorrect + '</h3>');
-        gameArea.append('<h3>Left Blank: ' + (questions.length - (quiz.incorrect + quiz.correct)) + '</h3>');
-        gameArea.append("<br><button id='restart'>Think You Could Do Better?</button>");
+        gameArea.html("<h2>Well done, here's your final score!</h2>");
+        $('#numeric-counter').html(quiz.counter);
+        gameArea.append('<h3>Correct Answers: ' + quiz.rightAnswer + '</h3>');
+        gameArea.append('<h3>Incorrect Answers: ' + quiz.wrongAnswer + '</h3>');
+        gameArea.append('<h3>Left Blank: ' + (questions.length - (quiz.wrongAnswer + quiz.rightAnswer)) + '</h3>');
+        gameArea.append('<br><button id="restart">Think You Could Do Better?</button>');
     },
     clicked: function (e) {
         clearInterval(timer);
 
         if ($(e.target).data("name") === questions
         [this.currentQuestion].rightAnswer) {
-            this.correctAnswer();
+            this.rightAnswer();
         } else {
             this.wrongAnswer();
         }
@@ -147,20 +144,20 @@ var quiz = {
     wrongAnswer: function () {
         clearInterval(timer);
         quiz.incorrect++;
-        gameArea.html('<h2>Sorry, Try Again!</h2>');
-        gameArea.append('<h3>The Right Answer is: ' + questions[quiz.currentQuestion].rightAnswer + '</h3>');
+        gameArea.html("<h2>Sorry, Try Again!</h2>");
+        gameArea.append("<h3>The Right Answer is: " + questions[quiz.currentQuestion].rightAnswer + "</h3>");
         gameArea.append('<img src="' + questions[quiz.currentQuestion].image + '" />');
 
         if (quiz.currentQuestion === questions.length - 1) {
-            setTimeout(quiz.results, 3000);
+            setTimeout(game.results, 3000);
         } else {
             setTimeout(quiz.nextQuestion, 3000);
         }
     },
-    correctAnswer: function () {
+    rightAnswer: function () {
         clearInterval(timer);
         quiz.correct++;
-        gameArea.html('<h2>You Are Correct!</h2>');
+        gameArea.html("<h2>You're Right!</h2>");
         gameArea.append('<img src="' + questions[quiz.currentQuestion].image + '" />');
 
         if (quiz.currentQuestion === questions.length - 1) {
@@ -171,7 +168,7 @@ var quiz = {
     },
     reset: function () {
         this.currentQuestion = 0;
-        this.counter = timer;
+        this.counter = startCounter;
         this.correct = 0;
         this.incorrect = 0;
         this.startTrivia();
